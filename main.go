14dbc2bcb559426b490afd86be61ac25fffa0b83
Jhonny <ojhonny1@hotmail.com>
2025-10-07 21:08:55 -0400

    Add health check endpoint and configure HTTP server

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+	"time"
+
 	"github.com/jjhonny/fem-project-go/internal/app"
 )
 
@@ -11,4 +15,22 @@ func main() {
 	}
 
 	app.Logger.Println("we are running our app")
+
+	http.HandleFunc("/health", HealthCheck)
+
+	server := &http.Server{
+		Addr:         ":8080",
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	err = server.ListenAndServe()
+	if err != nil {
+		app.Logger.Fatal(err)
+	}
+}
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Status is available\n")
 }
