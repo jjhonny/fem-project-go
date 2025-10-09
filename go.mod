5c4baca7f925ffe1f83d03e1c47379eeaf5fe5a0
Jhonny <ojhonny1@hotmail.com>
2025-10-08 20:34:29 -0400

    Add Chi router for HTTP handling and implement health check endpoint

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jjhonny/fem-project-go
 
 go 1.25.0
+
+require github.com/go-chi/chi/v5 v5.2.3
--- main.go
+++ main.go
@@ -1,36 +1,39 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/jjhonny/fem-project-go/internal/app"
+	"github.com/jjhonny/fem-project-go/internal/routes"
 )
 
 func main() {
+	var port int
+	flag.IntVar(&port, "port", 8080, "go backend server port")
+	flag.Parse()
+
 	app, err := app.NewApplication()
 	if err != nil {
 		panic(err)
 	}
 
-	app.Logger.Println("we are running our app")
-
-	http.HandleFunc("/health", HealthCheck)
+	r := routes.SetupRoutes(app)
 
 	server := &http.Server{
-		Addr:         ":8080",
+		Addr:         fmt.Sprintf(":%d", port),
+		Handler:      r,
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
 
+	app.Logger.Printf("we are running on port %d\n", port)
+
 	err = server.ListenAndServe()
 	if err != nil {
 		app.Logger.Fatal(err)
 	}
-}
-
-func HealthCheck(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Status is available\n")
 }
